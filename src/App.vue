a04f64639d2d04c0ca11d55676bcc6caaf1f8d56
MDanielle007 <marc.danielle.cabatay219@gmail.com>
2023-10-10 20:34:25 +0800

    Add Sidebar and App bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+    v-model="drawer"
+    permanent
+    theme="dark"
+  >
+    <v-list-item
+      prepend-icon="mdi-account"
+      title="Admin"
+    ></v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home" to="/"></v-list-item>
+      <v-list-item prepend-icon="mdi-table" title="Manage" value="manage" to="/manage"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+  <v-app-bar theme="dark">
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <template v-slot:append>
+      <v-btn icon="mdi-heart"></v-btn>
+
+      <v-btn icon="mdi-magnify"></v-btn>
+
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
+  </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
@@ -12,7 +39,7 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: !null,
   }),
 }
 </script>
